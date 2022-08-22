7b1638c24d1e4493bf58453e1609fac684e5da0a
hcaujolle <herve.caujolle@gmail.com>
2022-08-22 09:21:09 +0200

    dd

--- 2000_Simon Dieny React/00_dev/react-pokemons-app/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FunctionComponent} from "react";
-import PokemonList from "./pages/pokemon-list";
-
-const App: FunctionComponent = () => {
-  return <PokemonList />;
-};
-
-export default App;
